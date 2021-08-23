7df2d972aaad72b6b112b565065066510f4ac76d
Amrit Pandey <amrit.panday@coxautoinc.com>
2021-08-23 16:58:35 +0530

    Image loader class re-structred

--- Sources/KrakenCommon/Utilities/ImageLoader.swift
+++ Sources/KrakenCommon/Utilities/ImageLoader.swift
@@ -3,22 +3,25 @@
 //
 
 import Foundation
+import UIKit
 import Combine
 
 public class ImageLoader: ObservableObject {
-    public var didChange = PassthroughSubject<Data, Never>()
-    public var data = Data() {
-        didSet {
-            didChange.send(data)
-        }
-    }
+    @Published public var image: UIImage
 
-    public init(urlString:String) {
+    public init(urlString: String = "", placeHolder: UIImage?) {
+        _image = .init(initialValue: placeHolder ?? UIImage())
+        downloadImage(urlString: urlString)
+    }
+    
+    public func downloadImage(urlString: String) {
         guard let url = URL(string: urlString) else { return }
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data else { return }
             DispatchQueue.main.async {
-                self.data = data
+                if let image = UIImage(data: data) {
+                    self.image = image
+                }
             }
         }
         task.resume()
